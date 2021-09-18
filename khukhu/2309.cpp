b2d5477d36bc3ca6e5d0fd9d196aae4ebf0833b6
ShinjeongA <jeonga@khu.ac.kr>
2021-09-18 21:22:37 +0900

    일곱난쟁이

---
+++ khukhu/2309.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define FASTIO std::ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+int arr[10];
+int main() {
+	FASTIO;
+
+	int sum = 0;
+	for (int i = 1; i < 10; i++) {
+		int height;
+		cin >> height;
+		sum += height;
+
+		arr[i] = height;
+	}
+
+	sort(arr + 1, arr + 10); //오름차순 정렬
+	
+	int fake1 ;
+	int fake2;
+
+	bool flag = true;
+	int i = 1;
+	bool flag_2 = true;
+
+	while (flag) {
+		int liar;
+		for (int j = i + 1; j < 10; j++) {
+			liar = arr[i] + arr[j];
+			fake1 = i;
+			fake2 = j;
+			if (sum - liar == 100) {
+				flag_2 = false;
+				break;
+			}
+		}
+
+		if (flag_2 == false) {
+			break;
+		}
+		else {
+			i++;
+			if (i > 9) flag = false;
+
+		}
+	}
+
+
+	for (int i = 1; i < 10; i++) {
+		if (i != fake1 && i != fake2) {
+			cout << arr[i] << '\n';
+
+		}
+	}
+
+
+
+}
